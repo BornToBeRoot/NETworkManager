b3b47578a69b4f1507dc677f8a8ec5491bdd7953
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2017-06-01 23:32:17 +0200

    #7 & #6  fixed, code cleanup, send feedback added

--- Source/NETworkManager/Models/Network/WakeOnLan.cs
+++ Source/NETworkManager/Models/Network/WakeOnLan.cs
@@ -1,29 +1,17 @@
-﻿using System;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 
 namespace NETworkManager.Models.Network
 {
-    public class WakeOnLAN
+    public static class WakeOnLAN
     {
-        #region Events
-        public event EventHandler Completed;
-        
-        protected virtual void OnCompleted()
-        {
-            Completed?.Invoke(this, System.EventArgs.Empty);
-        }
-        #endregion
-
         #region Methods
-        public void Send(WakeOnLANInfo info)
+        public static void Send(WakeOnLANInfo info)
         {
             using (UdpClient udpClient = new UdpClient())
             {
                 udpClient.Connect(info.Broadcast, info.Port);
 
                 udpClient.Send(info.MagicPacket, info.MagicPacket.Length);
-
-                OnCompleted();
             }
         }
         #endregion
