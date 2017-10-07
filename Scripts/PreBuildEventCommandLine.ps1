c66d70930f578fec1f7e982023308e33e22942dd
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2017-10-07 23:39:41 +0200

    Create MSTSCLib.dll and AxMSTSCLib.dll with pre-build event command line

---
+++ Scripts/PreBuildEventCommandLine.ps1
@@ -0,0 +1,39 @@
+param(
+    [Parameter(
+        Position=0,
+        Mandatory=$true)]
+    [String]$OutPath
+)
+
+# Test if files are already there...
+if((Test-Path -Path "$OutPath\MSTSCLib.dll") -and (Test-Path -Path "$OutPath\AxMSTSCLib.dll"))
+{
+    Write-Host "MSTSCLib.dll and AxMSTSCLib.dll already created!"
+
+    exit 0
+}
+
+# x86 or x64
+$ProgramFiles_Path = ${Env:ProgramFiles(x86)}
+
+if([String]::IsNullOrEmpty($ProgramFiles_Path))
+{
+    $ProgramFiles_Path = $Env:ProgramFiles    
+}
+
+# Get aximp from sdk
+$files = (Get-ChildItem -Path "$ProgramFiles_Path\Microsoft SDKs\Windows" -Recurse -Filter "aximp.exe" -File)
+
+if($files.Count -eq 0)
+{
+    Write-Host "Aximp.exe not found on this system!"
+    exit 2
+}
+
+# Change location
+Write-Host "Change location to: $OutPath"
+Set-Location -Path $OutPath 
+
+# Create MSTSCLib.dll and AxMSTSCLib.dll
+Write-Host "Creating MSTSCLib.dll and AxMSTSCLib.dll ..."
+Start-Process -FilePath $files[$files.Length -1].FullName -ArgumentList "$($Env:windir)\system32\mstscax.dll" -Wait -NoNewWindow
