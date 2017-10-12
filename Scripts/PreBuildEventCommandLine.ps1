cbbdadca148e5b5d5d552abed37af3128306d952
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2017-10-12 11:29:16 +0200

    Fox wrong path (with/without spaces)

--- Scripts/PreBuildEventCommandLine.ps1
+++ Scripts/PreBuildEventCommandLine.ps1
@@ -5,6 +5,14 @@ param(
     [String]$OutPath
 )
 
+# Fix wrong path
+# Quotation marks are required if a blank is in the path... If there is no blank space in the path, a quote will be add to the end...
+# VS prebuild event call: PowerShell.exe -ExecutionPolicy Bypass -NoProfile -File "$(ProjectDir)..\..\Scripts\PreBuildEventCommandLine.ps1" "$(TargetDir)"
+if(-not($OutPath.StartsWith('"')))
+{
+    $OutPath = $OutPath.TrimEnd('"')
+}
+
 # Test if files are already there...
 if((Test-Path -Path "$OutPath\MSTSCLib.dll") -and (Test-Path -Path "$OutPath\AxMSTSCLib.dll"))
 {
