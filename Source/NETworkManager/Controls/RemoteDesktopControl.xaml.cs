2326e62c6920bcdfe21730e0f9c115f157dd6981
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2017-10-29 03:03:42 +0100

    Some changes

--- Source/NETworkManager/Controls/RemoteDesktopControl.xaml.cs
+++ Source/NETworkManager/Controls/RemoteDesktopControl.xaml.cs
@@ -8,18 +8,34 @@ namespace NETworkManager.Controls
         public RemoteDesktopControl(RemoteDesktopSessionInfo info)
         {
             InitializeComponent();
-            
-            mstsc.Server = info.Hostname;
 
-            // Auth
-            mstsc.AdvancedSettings9.EnableCredSspSupport = true;
+            rdpClient.Server = info.Hostname;
+
+            // AdvancedSettings
+            rdpClient.AdvancedSettings9.AuthenticationLevel = 2;
+            rdpClient.AdvancedSettings9.EnableCredSspSupport = true;
+            rdpClient.AdvancedSettings9.RedirectClipboard = false;
+            rdpClient.AdvancedSettings9.RedirectDevices = false;
+            rdpClient.AdvancedSettings9.RedirectDrives = false;
+            rdpClient.AdvancedSettings9.RedirectPorts = false;
+            rdpClient.AdvancedSettings9.RedirectSmartCards = false;
+            rdpClient.AdvancedSettings9.RedirectPrinters = false;
 
             // Display
-            mstsc.DesktopHeight = 768;
-            mstsc.DesktopWidth = 1280;
+            rdpClient.ColorDepth = 24;      // 8, 15, 16, 24
+            rdpClient.DesktopHeight = 768;
+            rdpClient.DesktopWidth = 1280;
 
+            // Events
+            rdpClient.OnDisconnected += Mstsc_OnDisconnected;
 
-            mstsc.Connect();
+            rdpClient.Connect();
+        }
+
+        private void Mstsc_OnDisconnected(object sender, AxMSTSCLib.IMsTscAxEvents_OnDisconnectedEvent e)
+        {
+            
+             // Add DC reason
         }
     }
 }
