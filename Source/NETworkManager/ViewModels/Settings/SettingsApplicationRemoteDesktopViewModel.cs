5c3c17fcde3bf9f2dca4f0f475cd126144b6c989
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2017-10-30 17:09:12 +0100

    Settings view added for remote desktop

---
+++ Source/NETworkManager/ViewModels/Settings/SettingsApplicationRemoteDesktopViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Heijden.DNS;
+using NETworkManager.Models.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NETworkManager.ViewModels.Settings
+{
+    public class SettingsApplicationRemoteDesktopViewModel : ViewModelBase
+    {
+        #region Variables
+        private bool _isLoading = true;
+
+       
+        #endregion
+
+        #region Constructor, load settings
+        public SettingsApplicationRemoteDesktopViewModel()
+        {
+            LoadSettings();
+
+            _isLoading = false;
+        }
+
+        private void LoadSettings()
+        {
+            
+        }
+        #endregion
+    }
+}
