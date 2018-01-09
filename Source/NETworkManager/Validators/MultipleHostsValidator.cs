f18e07e216cb1963b553eb1e305b8bfd998184ad
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2018-01-09 22:01:13 +0100

    Validate hosts

---
+++ Source/NETworkManager/Validators/MultipleHostsValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NETworkManager.Validators
+{
+    public class MultipleHostsValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            foreach(string host in (value as string).Split(';'))
+            {
+                if (string.IsNullOrEmpty(host))
+                    return new ValidationResult(false, Application.Current.Resources["String_ValidateError_EnterValidHosts"] as string);
+            }                
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
