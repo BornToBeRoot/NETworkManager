a651a332bd29ec0c0ffb783741f7643ca2e6cb58
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2018-05-16 19:04:45 +0200

    Choco package added

---
+++ Choco/NETworkManager/tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'NETworkManager_v1.6.2.0_Setup.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  file          = $fileLocation
+
+  softwareName  = 'NETworkManager*'
+
+  checksum      = '9DBED1062CCC8B99E682227C31B7E272CBA126B700CE5A0095D1AAD9ABE4CDDB'
+  checksumType  = 'sha256'
+  checksum64    = '9DBED1062CCC8B99E682227C31B7E272CBA126B700CE5A0095D1AAD9ABE4CDDB'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
