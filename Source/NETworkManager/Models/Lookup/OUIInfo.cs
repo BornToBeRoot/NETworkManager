7e5ff1c29e00a70d20dacb6e9f695e8cacba3f25
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2018-12-30 04:32:14 +0100

    Fix datagrid sort

--- Source/NETworkManager/Models/Lookup/OUIInfo.cs
+++ Source/NETworkManager/Models/Lookup/OUIInfo.cs
@@ -5,6 +5,8 @@
         public string MACAddress { get; set; }
         public string Vendor { get; set; }
 
+        public string MACAddressString => MACAddress.Replace("-", "").Replace(":", "");
+
         public OUIInfo(string macAddress, string vendor)
         {
             MACAddress = macAddress;
