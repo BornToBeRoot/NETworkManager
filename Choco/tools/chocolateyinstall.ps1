c41415f9ba7c4300ebb3dc3fca8f454a9395acf8
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2019-04-09 23:27:00 +0200

    v1.10.0.0

--- Choco/tools/chocolateyinstall.ps1
+++ Choco/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/BornToBeRoot/NETworkManager/releases/download/v1.9.0.0/NETworkManager_v1.9.0.0_Setup.msi'  
+$url        = 'https://github.com/BornToBeRoot/NETworkManager/releases/download/v1.10.0.0/NETworkManager_v1.10.0.0_Setup.msi'  
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'NETworkManager*'
 
-  checksum      = '2C01DE0C0B0F01D92062AAE9F3F3C9EB46887D5D7A9CC1E9ADBA3512C07D2EF5'
+  checksum      = 'FD9D9E4A3BBCFE8599148D4238A83D406D74FA17E67691A9ED4107AB31E142DA'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
