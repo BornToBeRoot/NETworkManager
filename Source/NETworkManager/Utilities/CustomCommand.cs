ba1a9734411e4bcfbb58085dfc17333a56fa1600
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2019-07-30 19:17:53 +0200

    CustomCommand added (explorer c$)

--- Source/NETworkManager/Utilities/CustomCommand.cs
+++ Source/NETworkManager/Utilities/CustomCommand.cs
@@ -10,8 +10,9 @@ namespace NETworkManager.Utilities
         {
             return new List<CustomCommandInfo>
             {
-                new CustomCommandInfo(Guid.NewGuid(), "Internet Explorer", "iexplore.exe", "http://$$ipaddress$$/"),
-                new CustomCommandInfo(Guid.NewGuid(), "Internet Explorer (https)", "iexplore.exe", "https://$$ipaddress$$/"),
+                new CustomCommandInfo(Guid.NewGuid(), "Internet Explorer", "iexplore.exe", @"http://$$ipaddress$$/"),
+                new CustomCommandInfo(Guid.NewGuid(), "Internet Explorer (https)", "iexplore.exe", @"https://$$ipaddress$$/"),
+                new CustomCommandInfo(Guid.NewGuid(), "Windows Explorer (c$)", "explorer.exe", @"\\$$ipaddress$$\c$"),
             };
         }
 
