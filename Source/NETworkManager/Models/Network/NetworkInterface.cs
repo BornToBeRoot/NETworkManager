edf4708b83f3dd77d822b74b77fefc1e6f18309a
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2019-08-09 23:14:32 +0200

    Cleanup

--- Source/NETworkManager/Models/Network/NetworkInterface.cs
+++ Source/NETworkManager/Models/Network/NetworkInterface.cs
@@ -3,7 +3,6 @@ using NETworkManager.Utilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
