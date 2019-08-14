a31c505ed3f2253c91ff9e2244518fbde08455cb
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2019-08-14 23:16:18 +0200

    ListViewItem --> Expand details

--- Source/NETworkManager/Models/Network/WiFiNetworkInfo.cs
+++ Source/NETworkManager/Models/Network/WiFiNetworkInfo.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Windows.Devices.WiFi;
 using Windows.Networking.Connectivity;
 
 namespace NETworkManager.Models.Network
 {
-    public class WiFiNetworkInfo
+    public class WiFiNetworkInfo : INotifyPropertyChanged
     {
+        #region PropertyChangedEventHandler
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Variables
+        #region WiFiNetwork
         public string BSSID { get; set; }
         public string SSID { get; set; }
         public int ChannelCenterFrequencyInKilohertz { get; set; }
@@ -17,6 +30,24 @@ namespace NETworkManager.Models.Network
         public NetworkSecuritySettings Security { get; set; }
         public TimeSpan BeaconInterval { get; set; }
         public TimeSpan Uptime { get; set; }
+        #endregion
+
+        #region View/Control
+        private bool _expandDetails;
+        public bool ExpandDetails
+        {
+            get => _expandDetails;
+            set
+            {      
+                if (value == _expandDetails)
+                    return;
+
+                _expandDetails = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        #endregion
 
         public WiFiNetworkInfo()
         {
