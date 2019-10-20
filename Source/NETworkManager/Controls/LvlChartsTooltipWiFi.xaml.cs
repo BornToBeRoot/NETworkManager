27ce485b5f4819d448f2d7a4c0e6e16226220008
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2019-10-21 00:12:28 +0200

    WiFi ToolTip Channel added

--- Source/NETworkManager/Controls/LvlChartsTooltipWiFi.xaml.cs
+++ Source/NETworkManager/Controls/LvlChartsTooltipWiFi.xaml.cs
@@ -1,22 +1,8 @@
 ﻿using LiveCharts;
 using LiveCharts.Wpf;
-using NETworkManager.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace NETworkManager.Controls
 {
---
+++ Source/NETworkManager/Converters/WiFiTooltipChannelConverter.cs
@@ -0,0 +1,30 @@
+﻿using LiveCharts.Wpf;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NETworkManager.Converters
+{
+    public sealed class WiFiTooltipChannelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TooltipData info)
+            {
+                double index = info.SharedValue ?? -1;
+
+                if (index == -1)
+                    return "-/-";
+
+                return info.XFormatter.Invoke(index) as string;
+            }
+
+            return "-/-";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
