7981f19326d2277b90be11d88c4e3cbf7d1b74f9
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2020-03-24 22:44:40 +0100

    Update

--- Source/NETworkManager.Models/RemoteDesktop/AudioCaptureRedirectionMode.cs
+++ Source/NETworkManager.Models/RemoteDesktop/AudioCaptureRedirectionMode.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NETworkManager.Models.RemoteDesktop
+﻿namespace NETworkManager.Models.RemoteDesktop
 {
+    /// <summary>
+    /// Enum indicates whether the default audio input is redirected from the client to the remote session.
+    /// See also: https://docs.microsoft.com/en-us/windows/win32/termserv/imsrdpclientadvancedsettings7-audiocaptureredirectionmode
+    /// </summary>
     public enum AudioCaptureRedirectionMode
     {
+        /// <summary>
+        /// Redirect the default audio input device to the remote session.
+        /// </summary>
         RecordFromThisComputer,
+
+        /// <summary>
+        /// Don't redirect the default audio input device to the remote session.
+        /// </summary>
         DoNotRecord
     }
 }
--- Source/NETworkManager.Models/RemoteDesktop/Keystroke.cs
+++ Source/NETworkManager.Models/RemoteDesktop/Keystroke.cs
@@ -1,7 +1,13 @@
 ﻿namespace NETworkManager.Models.RemoteDesktop
 {
+    /// <summary>
+    /// Enum represents all possible keystrokes which can be send into the remote session.
+    /// </summary>
     public enum Keystroke
     {
+        /// <summary>
+        /// Ctrl + Alt + Del keystroke.
+        /// </summary>
         CtrlAltDel
     }
 }
