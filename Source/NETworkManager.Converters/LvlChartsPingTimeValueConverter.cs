da237c961bed6835b38f503f087be48b6343b3f3
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2020-04-29 22:45:40 +0200

    Close #286

---
+++ Source/NETworkManager.Converters/LvlChartsPingTimeValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using NETworkManager.Utilities;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NETworkManager.Converters
+{
+    public sealed class LvlChartsPingTimeValueConverter : IValueConverter
+    {
+ 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">ChartPoint.Instance (object)</param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value is LvlChartsPingTimeInfo info)
+                return $"{info.Value} ms";
+
+            return "-/-";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
