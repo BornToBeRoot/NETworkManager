1f35a12f127ac5843ed6a1c515af22fa5d3cdce0
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2020-08-31 13:26:23 +0200

    Global assembly fixed

--- Source/NETworkManager.Settings/AssemblyManager.cs
+++ Source/NETworkManager.Settings/AssemblyManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 
 namespace NETworkManager.Settings
 {
@@ -18,6 +19,8 @@ namespace NETworkManager.Settings
                 Location = assembly.Location,
                 Name = name.Name
             };
+
+            Debug.WriteLine(Current);
         }
     }
 }
