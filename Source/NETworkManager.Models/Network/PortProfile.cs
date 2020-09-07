93c7ebdb8ccadfd7e91c82f6f7b8ceda92dee3c2
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2020-09-07 02:19:25 +0200

    Typo fixed

--- Source/NETworkManager.Models/Network/PortProfile.cs
+++ Source/NETworkManager.Models/Network/PortProfile.cs
@@ -10,7 +10,7 @@ namespace NETworkManager.Models.Network
             {                
                 new PortProfileInfo("Webserver", "80; 443"),
                 new PortProfileInfo("Remote access", "22; 23; 3389"),
-                new PortProfileInfo("Mailserver", "25; 110; 143; 465; 587 993; 995"),
+                new PortProfileInfo("Mailserver", "25; 110; 143; 465; 587; 993; 995"),
                 new PortProfileInfo("Filetransfer", "21; 22; 69; 989-990"),
                 new PortProfileInfo("DNS server", "53"),
                 new PortProfileInfo("SQL server", "1433-1434"),
