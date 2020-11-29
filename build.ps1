a545ea5ca4b8a0c72aca8d5fda35e26b123ef04f
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2020-11-29 07:07:36 +0100

    Fix build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$Version = "2020.9.0"
+$Version = "2020.12.0"
 $IsPreview = $true
 
 $BuildPath = "$PSScriptRoot\Build"
@@ -8,12 +8,13 @@ if(Test-Path -Path $BuildPath)
     Remove-Item $BuildPath -Recurse
 }
 
-# Dotnet restore and build
+# Dotnet clean, restore and build
+dotnet clean "$PSScriptRoot\Source\NETworkManager.sln"
 dotnet restore "$PSScriptRoot\Source\NETworkManager.sln"
 dotnet build --configuration Release "$PSScriptRoot\Source\NETworkManager.sln"
 
 # Copy files
-Copy-Item -Recurse -Path "$PSScriptRoot\Source\NETworkManager\bin\Release\net5.0-windows" -Destination "$BuildPath\NETworkManager"
+Copy-Item -Recurse -Path "$PSScriptRoot\Source\NETworkManager\bin\Release\net5.0-windows10.0.17763.0" -Destination "$BuildPath\NETworkManager"
 
 # Is preview?
 if($IsPreview)
@@ -28,3 +29,7 @@ Compress-Archive -Path "$BuildPath\NETworkManager" -DestinationPath "$BuildPath\
 New-Item -Path "$BuildPath\NETworkManager" -Name "IsPortable.settings" -ItemType File
 Compress-Archive -Path "$BuildPath\NETworkManager" -DestinationPath "$BuildPath\NETworkManager_$($Version)_Portable.zip"
 Remove-Item -Path "$BuildPath\NETworkManager\IsPortable.settings"
+
+# Installer Build...
+
+Write-Host "Your build is here: $BuildPath "
