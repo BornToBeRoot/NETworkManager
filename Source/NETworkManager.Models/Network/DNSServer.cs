3cd8d396114a6d36cad6e77c0df3281bb6efdc49
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2020-12-14 01:32:45 +0100

    Fix #390 - Use custom dns resolver (#395)
    * NetworkInterfaceType 53 added - Close #375
    
    * Close #390
    
    * Code improved, documentation added
    
    Co-authored-by: BornToBeRoot <BornToBeRoot@users.noreply.github.com>

--- Source/NETworkManager.Models/Network/DNSServer.cs
+++ Source/NETworkManager.Models/Network/DNSServer.cs
@@ -2,13 +2,20 @@
 
 namespace NETworkManager.Models.Network
 {
+    /// <summary>
+    /// Class provides static informations about dns servers.
+    /// </summary>
     public static class DNSServer
     {
+        /// <summary>
+        /// Method will return a default list of common dns servers.
+        /// </summary>
+        /// <returns>List of common dns servers.</returns>
         public static List<DNSServerInfo> DefaultList()
         {
             return new List<DNSServerInfo>
             {
-                new DNSServerInfo { UseWindowsDNSServer = true },
+                new DNSServerInfo(), // Windows DNS server
                 new DNSServerInfo("Cloudflare", new List<string>{"1.1.1.1", "1.0.0.1"}),
                 new DNSServerInfo("DNS.Watch", new List<string>{"84.200.69.80", "84.200.70.40"}),
                 new DNSServerInfo("Google Public DNS", new List<string>{"8.8.8.8", "8.8.4.4"}),
