d4165a0d871d676983a0641d1e4abfda53583f80
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2020-12-28 05:27:59 +0100

    #366 - Fix y-axis, add space between channel 64 and 100

---
+++ Source/NETworkManager.Converters/WiFiDBMReverseConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NETworkManager.Converters
+{
+    public sealed class WiFiDBMReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is double))
+                return "-/-";
+
+            return $"-{100 - (double)value} dBm";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
