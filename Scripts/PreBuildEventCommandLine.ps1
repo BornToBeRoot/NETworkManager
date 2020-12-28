f8a9bf2426aff495f803daa2deff9b8260247f10
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2020-12-28 01:52:38 +0100

    Fix blank in path

--- Scripts/PreBuildEventCommandLine.ps1
+++ Scripts/PreBuildEventCommandLine.ps1
@@ -1,18 +1,15 @@
+<#
+    Visual Studio prebuild event command line
+    
+    PowerShell.exe -ExecutionPolicy Bypass -NoProfile -File "$(ProjectDir)..\..\Scripts\PreBuildEventCommandLine.ps1" "$(TargetDir)"
+#> 
+
 param(
     [Parameter(
         Position=0,
         Mandatory=$true)]
     [String]$OutPath
 )
-
-# VS prebuild event call: PowerShell.exe -ExecutionPolicy Bypass -NoProfile -File "$(ProjectDir)..\..\Scripts\PreBuildEventCommandLine.ps1" "$(TargetDir)"
-
-# Fix wrong path
-# Quotation marks are required if a blank is in the path... If there is no blank space in the path, a quote will be add to the end...
-if(-not($OutPath.StartsWith('"')))
-{
-    $OutPath = $OutPath.TrimEnd('"')
-}
 
 # Test if files are already there...
 if((Test-Path -Path "$OutPath\MSTSCLib.dll") -and (Test-Path -Path "$OutPath\AxMSTSCLib.dll"))
@@ -34,11 +31,29 @@ $AximpPath = ((Get-ChildItem -Path "$ProgramFiles_Path\Microsoft SDKs\Windows" -
 $IldasmPath = ((Get-ChildItem -Path "$ProgramFiles_Path\Microsoft SDKs\Windows" -Recurse -Filter "ildasm.exe" -File) | Sort-Object CreationTime | Select-Object -First 1).FullName
 $IlasmPath = ((Get-ChildItem -Path "$($Env:windir)\Microsoft.NET\Framework\" -Recurse -Filter "ilasm.exe" -File) | Sort-Object CreationTime | Select-Object -First 1).FullName
 
-if([String]::IsNullOrEmpty($AximpPath) -or [String]::IsNullOrEmpty($IldasmPath) -or [String]::IsNullOrEmpty($IlasmPath))
+if([String]::IsNullOrEmpty($AximpPath))
 {
-    Write-Host "Could not find sdk tools:`naximp.exe`t$AximpPath`nildasm.exe`t$IldasmPath`nilasm.exe`t$IlasmPath"
-
+    Write-Host "Could not find: aximp.exe"
     return
+}
+
+if([String]::IsNullOrEmpty($IldasmPath))
+{
+    Write-Host "Could not find: ildasm.exe"
+    return
+}
+
+if([String]::IsNullOrEmpty($IlasmPath))
+{
+    Write-Host "Could not find: ilasm.exe"
+    return
+}
+
+# Create output folder
+if(-not(Test-Path $OutPath))
+{
+    Write-Host "Creating directory $OutPath" 
+    New-Item -Path $OutPath -ItemType Directory | Out-Null
 }
 
 # Change location to output folder...
@@ -54,18 +69,24 @@ Start-Process -FilePath $AximpPath -ArgumentList "$($Env:windir)\system32\mstsca
 Write-Host "Modify MSTSCLib.dll..."
 Write-Host "Using ildasm.exe from: $IldasmPath"
 
-$MSTSCLibDLLPath = "$OutPath\MSTSCLib.dll"
-$MSTSCLibILPath = "$OutPath\MSTSCLib.il"
+# Create paths
+$MSTSCLibDLLPath = Join-Path -Path $OutPath -ChildPath "MSTSCLib.dll"
+$MSTSCLibILPath = Join-Path -Path $OutPath -ChildPath "MSTSCLib.il"
 
-Start-Process -FilePath $IldasmPath -ArgumentList "$MSTSCLibDLLPath /out=$MSTSCLibILPath" -Wait -NoNewWindow
+# Decompile
+Start-Process -FilePath $IldasmPath -ArgumentList """$MSTSCLibDLLPath"" /out=""$MSTSCLibILPath""" -Wait -NoNewWindow
 
+# Modify - part 1
 Write-Host "Replace ""[in] bool& pbArrayKeyUp"" with ""[in] bool[] marshal([+0]) pbArrayKeyUp"""
 (Get-Content -Path $MSTSCLibILPath).Replace("[in] bool& pbArrayKeyUp", "[in] bool[] marshal([+0]) pbArrayKeyUp") | Set-Content -Path $MSTSCLibILPath
 
+# Modify - part 2
 Write-Host "Replace ""[in] int32& plKeyData"" with ""[in] int32[] marshal([+0]) plKeyData"""
 (Get-Content -Path $MSTSCLibILPath).Replace("[in] int32& plKeyData", "[in] int32[] marshal([+0]) plKeyData") | Set-Content -Path $MSTSCLibILPath
 
-Start-Process -FilePath $IlasmPath -ArgumentList "/dll $MSTSCLibILPath /output:$MSTSCLibDllPath" -Wait -NoNewWindow
+# Compile
+Start-Process -FilePath $IlasmPath -ArgumentList "/dll ""$MSTSCLibILPath"" /output:""$MSTSCLibDllPath""" -Wait -NoNewWindow
 
-Remove-Item -Path "$MSTSCLibILPath"
-Remove-Item -Path "$OutPath\MSTSCLib.res"
+# Cleanup
+Remove-Item -Path $MSTSCLibILPath
+Remove-Item -Path (Join-Path -Path $OutPath -ChildPath "MSTSCLib.res")
