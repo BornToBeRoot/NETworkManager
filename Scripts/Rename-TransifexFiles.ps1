a5cc69acd3a4036b5889e8838a19928018d176d8
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2020-12-29 23:18:07 +0100

    Update Rename-TransifexFiles.ps1

--- Scripts/Rename-TransifexFiles.ps1
+++ Scripts/Rename-TransifexFiles.ps1
@@ -1,4 +1,10 @@
-﻿# Rename language files from transifex
+﻿<#  
+    Rename language files from transifex from
+    "Strings_en_US.resx" to "Strings.en-US.resx"
+
+    Unzip the folder and call
+    ~# .\Rename-TransifexFiles.ps1 -FolderPath C:\Path\To\Folder
+#>
 
 [CmdletBinding()]
 param(
