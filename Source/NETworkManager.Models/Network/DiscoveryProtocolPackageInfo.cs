a30e8fe4faefd13674bae28e49315678ad69d549
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2021-05-02 23:39:14 +0200

    Fix/Improve DiscoveryProtocol (#772)
    * Update script, fix binding, fix wrong values, add more values, remove network adapter selection
    
    * Update NETworkManager.Models.csproj
    
    * Update 04_Changelog.md
    
    Co-authored-by: BornToBeRoot <BornToBeRoot@users.noreply.github.com>

--- Source/NETworkManager.Models/Network/DiscoveryProtocolPackageInfo.cs
+++ Source/NETworkManager.Models/Network/DiscoveryProtocolPackageInfo.cs
@@ -9,14 +9,16 @@
         public string VLAN { get; private set; }
         public string IPAddress { get; private set; }
         public string Protocol { get; private set; }
-        public string Time { get; private set; }
+        public string TimeToLive { get; private set; }
+        public string Management { get; private set; }
+        public string ChassisId { get; private set; }
 
         public DiscoveryProtocolPackageInfo()
         {
 
         }
 
-        public DiscoveryProtocolPackageInfo(string device, string port, string description, string model, string vlan, string ipAdress, string protocol, string time)
+        public DiscoveryProtocolPackageInfo(string device, string port, string description, string model, string vlan, string ipAdress, string protocol, string timeToLive, string management, string chassisId)
         {
             Device = device;
             Port = port;
@@ -25,13 +27,14 @@
             VLAN = vlan;
             IPAddress = ipAdress;
             Protocol = protocol;
-            Time = time;
+            TimeToLive = timeToLive;
+            Management = management;
+            ChassisId = chassisId;
         }
 
         public static DiscoveryProtocolPackageInfo Parse(DiscoveryProtocolPackageArgs e)
         {
-            return new DiscoveryProtocolPackageInfo(e.Device, e.Protocol, e.Device, e.Model, e.VLAN, e.IPAddress, e.Protocol, e.Time);
+            return new DiscoveryProtocolPackageInfo(e.Device, e.Port, e.Description, e.Model, e.VLAN, e.IPAddress, e.Protocol, e.TimeToLive, e.Management, e.ChassisId);
         }
-
     }
 }
