ad4951eda0620c9c5e1c5a314eb660a6ce2c14cc
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2021-05-30 20:44:00 +0200

    IPv4 address regex for extract fixed (#842)
    * IPv4 address regex for extract fixed
    
    * #841 added
    
    Co-authored-by: BornToBeRoot <BornToBeRoot@users.noreply.github.com>

--- Source/NETworkManager.Utilities/RegexHelper.cs
+++ Source/NETworkManager.Utilities/RegexHelper.cs
@@ -8,7 +8,7 @@ namespace NETworkManager.Utilities
         /// <summary>
         /// Match an IPv4-Address like 192.168.178.1
         /// </summary>
-        private const string IPv4AddressValues = @"(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])";
+        private const string IPv4AddressValues = @"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
 
         /// <summary>
         /// Match exactly an IPv4-Address like 192.168.178.1 
