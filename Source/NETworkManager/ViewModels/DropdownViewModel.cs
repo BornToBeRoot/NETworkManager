56fe2c9e437d9a27b02cf565c8c288474af3a69a
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2021-06-26 16:23:04 +0200

    Use dropdown to remove ip address to prevent user errors

---
+++ Source/NETworkManager/ViewModels/DropdownViewModel.cs
@@ -0,0 +1,68 @@
+﻿using NETworkManager.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+namespace NETworkManager.ViewModels
+{
+    public class DropdownViewModel : ViewModelBase
+    {
+        public ICommand OKCommand { get; }
+
+        public ICommand CancelCommand { get; }
+
+        private string _valueDescription;
+        public string ValueDescription
+        {
+            get => _valueDescription;
+            set
+            {
+                if(value == _valueDescription)
+                    return;
+
+                _valueDescription = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<string> _values;
+        public List<string> Values
+        {
+            get => _values;
+            set
+            {
+                if (value == _values)
+                    return;
+
+                _values = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _selectedValue;
+        public string SelectedValue
+        {
+            get => _selectedValue;
+            set
+            {
+                if (value == _selectedValue)
+                    return;
+
+                _selectedValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DropdownViewModel(Action<DropdownViewModel> okCommand, Action<DropdownViewModel> cancelHandler, List<string> values, string valueDescription)
+        {
+            ValueDescription = valueDescription;
+            Values = values;
+
+            SelectedValue = Values.FirstOrDefault();
+
+            OKCommand = new RelayCommand(p => okCommand(this));
+            CancelCommand = new RelayCommand(p => cancelHandler(this));
+        }      
+    }
+}
