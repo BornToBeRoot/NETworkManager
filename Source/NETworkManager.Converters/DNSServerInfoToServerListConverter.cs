0458131992ee7675bfacd266063371885c15c396
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2021-08-09 23:33:26 +0200

    Show servers in settings view

--- Source/NETworkManager.Converters/DNSServerInfoToServerListConverter.cs
+++ Source/NETworkManager.Converters/DNSServerInfoToServerListConverter.cs
@@ -1,20 +1,26 @@
-﻿using System;
+﻿using NETworkManager.Models.Network;
+using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Windows.Data;
-using NETworkManager.Models.Network;
-using NETworkManager.Localization.Resources;
 
 namespace NETworkManager.Converters
 {
-    public sealed class DNSServerInfoToString : IValueConverter
+    public sealed class DNSServerInfoToServerListConverter : IValueConverter
     {
-        /* Convert an MahApps.Metro.Accent (from wpf/xaml-binding) to a Brush to fill rectangle with color in a ComboBox */
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is not DNSServerInfo info)
                 return "-/-";
 
-            return info.UseWindowsDNSServer ? $"[{Strings.WindowsDNSSettings}]" : info.Name;
+            List<string> list = new List<string>();
+
+            if (info.UseDoHServer)
+                info.DoHServers.ForEach(x => list.Add(x.URL));
+            else
+                info.Servers.ForEach(x => list.Add($"{x.Server}:{x.Port}"));
+
+            return string.Join("; ", list);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
