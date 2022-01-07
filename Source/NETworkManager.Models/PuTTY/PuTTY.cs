9a461decf7c7751a5ad6903f0a82e8edb02a995c
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2022-01-07 01:32:31 +0100

    Feature/putty/custom profile (#1358)
    * Disable light theme
    
    * Use own putty profile to make the integration prettier

--- Source/NETworkManager.Models/PuTTY/PuTTY.cs
+++ Source/NETworkManager.Models/PuTTY/PuTTY.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace NETworkManager.Models.PuTTY
@@ -8,6 +10,42 @@ namespace NETworkManager.Models.PuTTY
     /// </summary>
     public partial class PuTTY
     {
+        private static List<Tuple<string, string>> DefaultProfileRegkeysSZ = new()
+        {
+            new Tuple<string, string>("Colour0", "187,187,187"),
+            new Tuple<string, string>("Colour1", "255,255,255"),
+            new Tuple<string, string>("Colour2", "37,37,37"), // Background color
+            new Tuple<string, string>("Colour3", "85,85,85"),
+            new Tuple<string, string>("Colour4", "0,0,0"),
+            new Tuple<string, string>("Colour5", "0,255,0"),
+            new Tuple<string, string>("Colour6", "0,0,0"),
+            new Tuple<string, string>("Colour7", "85,85,85"),
+            new Tuple<string, string>("Colour8", "187,0,0"),
+            new Tuple<string, string>("Colour9", "255,85,85"),
+            new Tuple<string, string>("Colour10", "0,187,0"),
+            new Tuple<string, string>("Colour11", "85,255,85"),
+            new Tuple<string, string>("Colour12", "187,187,0"),
+            new Tuple<string, string>("Colour13", "255,255,85"),
+            new Tuple<string, string>("Colour14", "0,0,187"),
+            new Tuple<string, string>("Colour15", "85,85,255"),
+            new Tuple<string, string>("Colour16", "187,0,187"),
+            new Tuple<string, string>("Colour17", "255,85,255"),
+            new Tuple<string, string>("Colour18", "0,187,187"),
+            new Tuple<string, string>("Colour19", "85,255,255"),
+            new Tuple<string, string>("Colour20", "187,187,187"),
+            new Tuple<string, string>("Colour21", "255,255,255"),
+            new Tuple<string, string>("LineCodePage", "UTF-8"),
+            new Tuple<string, string>("Font", "Consolas")
+        };
+
+        private static List<Tuple<string, int>> DefaultProfileRegkeysDword = new()
+        {
+            new Tuple<string, int>("CurType", 2),
+            new Tuple<string, int>("FontHeight", 12),
+            new Tuple<string, int>("BlinkCur", 1),
+            new Tuple<string, int>("ScrollBar", 0)
+        };
+
         /// <summary>
         /// Build command line arguments based on a <see cref="PuTTYSessionInfo"/>.
         /// </summary>
@@ -81,6 +119,30 @@ namespace NETworkManager.Models.PuTTY
                 command += $" -P {sessionInfo.PortOrBaud} {sessionInfo.HostOrSerialLine}";
 
             return command;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void WriteDefaultProfileToRegistry()
+        {
+            string profilePath = @"Software\SimonTatham\PuTTY\Sessions\NETworkManager";
+
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(profilePath, true);
+
+            if (registryKey == null)
+                registryKey = Registry.CurrentUser.CreateSubKey(profilePath);
+
+            if (registryKey != null)
+            {
+                foreach (var key in DefaultProfileRegkeysSZ)
+                    registryKey.SetValue(key.Item1, key.Item2);
+
+                foreach (var key in DefaultProfileRegkeysDword)
+                    registryKey.SetValue(key.Item1, key.Item2);
+            }
+
+            registryKey.Close();
         }
     }
 }
