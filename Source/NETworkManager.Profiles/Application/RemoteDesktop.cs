560061b61189cf38c0d8647594acc1e6c73eff50
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2022-02-20 21:23:29 +0100

    Implement profile group logic for remote desktop

--- Source/NETworkManager.Profiles/Application/RemoteDesktop.cs
+++ Source/NETworkManager.Profiles/Application/RemoteDesktop.cs
@@ -56,101 +56,160 @@ namespace NETworkManager.Profiles.Application
             };
         }
 
-        public static RemoteDesktopSessionInfo CreateSessionInfo(ProfileInfo profileInfo)
+        public static RemoteDesktopSessionInfo CreateSessionInfo(ProfileInfo profile)
         {
             var info = CreateSessionInfo();
 
+            // Get group info
+            GroupInfo group = ProfileManager.GetGroup(profile.Group);
+
             // Override hostname
-            info.Hostname = profileInfo.Host;
+            info.Hostname = profile.Host;
 
             // Network
-            if (profileInfo.RemoteDesktop_OverridePort)
-                info.Port = profileInfo.RemoteDesktop_Port;
+            if (profile.RemoteDesktop_OverridePort)
+                info.Port = profile.RemoteDesktop_Port;
+            else if (group.RemoteDesktop_OverridePort)
+                info.Port = group.RemoteDesktop_Port;
 
             // Display
-            if (profileInfo.RemoteDesktop_OverrideDisplay)
+            if (profile.RemoteDesktop_OverrideDisplay)
             {
-                info.AdjustScreenAutomatically = profileInfo.RemoteDesktop_AdjustScreenAutomatically;
-                info.UseCurrentViewSize = profileInfo.RemoteDesktop_UseCurrentViewSize;
-                info.DesktopWidth = profileInfo.RemoteDesktop_UseCustomScreenSize ? profileInfo.RemoteDesktop_CustomScreenWidth : profileInfo.RemoteDesktop_ScreenWidth;
-                info.DesktopHeight = profileInfo.RemoteDesktop_UseCustomScreenSize ? profileInfo.RemoteDesktop_CustomScreenHeight : profileInfo.RemoteDesktop_ScreenHeight;
-                info.ColorDepth = profileInfo.RemoteDesktop_ColorDepth;
+                info.AdjustScreenAutomatically = profile.RemoteDesktop_AdjustScreenAutomatically;
+                info.UseCurrentViewSize = profile.RemoteDesktop_UseCurrentViewSize;
+                info.DesktopWidth = profile.RemoteDesktop_UseCustomScreenSize ? profile.RemoteDesktop_CustomScreenWidth : profile.RemoteDesktop_ScreenWidth;
+                info.DesktopHeight = profile.RemoteDesktop_UseCustomScreenSize ? profile.RemoteDesktop_CustomScreenHeight : profile.RemoteDesktop_ScreenHeight;
+                info.ColorDepth = profile.RemoteDesktop_ColorDepth;
+            }
+            else if (group.RemoteDesktop_OverrideDisplay)
+            {
+                info.AdjustScreenAutomatically = group.RemoteDesktop_AdjustScreenAutomatically;
+                info.UseCurrentViewSize = group.RemoteDesktop_UseCurrentViewSize;
+                info.DesktopWidth = group.RemoteDesktop_UseCustomScreenSize ? group.RemoteDesktop_CustomScreenWidth : group.RemoteDesktop_ScreenWidth;
+                info.DesktopHeight = group.RemoteDesktop_UseCustomScreenSize ? group.RemoteDesktop_CustomScreenHeight : group.RemoteDesktop_ScreenHeight;
+                info.ColorDepth = group.RemoteDesktop_ColorDepth;
             }
 
             // Authentication
-            if (profileInfo.RemoteDesktop_OverrideCredSspSupport)
-                info.EnableCredSspSupport = profileInfo.RemoteDesktop_EnableCredSspSupport;
+            if (profile.RemoteDesktop_OverrideCredSspSupport)
+                info.EnableCredSspSupport = profile.RemoteDesktop_EnableCredSspSupport;
+            else if (group.RemoteDesktop_OverrideCredSspSupport)
+                info.EnableCredSspSupport = group.RemoteDesktop_EnableCredSspSupport;
 
-            if (profileInfo.RemoteDesktop_OverrideAuthenticationLevel)
-                info.AuthenticationLevel = profileInfo.RemoteDesktop_AuthenticationLevel;
+            if (profile.RemoteDesktop_OverrideAuthenticationLevel)
+                info.AuthenticationLevel = profile.RemoteDesktop_AuthenticationLevel;
+            else if (group.RemoteDesktop_OverrideAuthenticationLevel)
+                info.AuthenticationLevel = group.RemoteDesktop_AuthenticationLevel;
 
             // Remote audio
-            if (profileInfo.RemoteDesktop_OverrideAudioRedirectionMode)
-                info.AudioRedirectionMode = profileInfo.RemoteDesktop_AudioRedirectionMode;
+            if (profile.RemoteDesktop_OverrideAudioRedirectionMode)
+                info.AudioRedirectionMode = profile.RemoteDesktop_AudioRedirectionMode;
+            else if (group.RemoteDesktop_OverrideAudioRedirectionMode)
+                info.AudioRedirectionMode = group.RemoteDesktop_AudioRedirectionMode;
 
-            if (profileInfo.RemoteDesktop_OverrideAudioCaptureRedirectionMode)
-                info.AudioCaptureRedirectionMode = profileInfo.RemoteDesktop_AudioCaptureRedirectionMode;
+            if (profile.RemoteDesktop_OverrideAudioCaptureRedirectionMode)
+                info.AudioCaptureRedirectionMode = profile.RemoteDesktop_AudioCaptureRedirectionMode;
+            else if (group.RemoteDesktop_OverrideAudioCaptureRedirectionMode)
+                info.AudioCaptureRedirectionMode = group.RemoteDesktop_AudioCaptureRedirectionMode;
 
             // Keyboard
-            if (profileInfo.RemoteDesktop_OverrideApplyWindowsKeyCombinations)
-                info.KeyboardHookMode = profileInfo.RemoteDesktop_KeyboardHookMode;
+            if (profile.RemoteDesktop_OverrideApplyWindowsKeyCombinations)
+                info.KeyboardHookMode = profile.RemoteDesktop_KeyboardHookMode;
+            else if (group.RemoteDesktop_OverrideApplyWindowsKeyCombinations)
+                info.KeyboardHookMode = group.RemoteDesktop_KeyboardHookMode;
 
             // Local devices and resources
-            if (profileInfo.RemoteDesktop_OverrideRedirectClipboard)
-                info.RedirectClipboard = profileInfo.RemoteDesktop_RedirectClipboard;
+            if (profile.RemoteDesktop_OverrideRedirectClipboard)
+                info.RedirectClipboard = profile.RemoteDesktop_RedirectClipboard;
+            else if (group.RemoteDesktop_OverrideRedirectClipboard)
+                info.RedirectClipboard = group.RemoteDesktop_RedirectClipboard;
 
-            if (profileInfo.RemoteDesktop_OverrideRedirectDevices)
-                info.RedirectDevices = profileInfo.RemoteDesktop_RedirectDevices;
+            if (profile.RemoteDesktop_OverrideRedirectDevices)
+                info.RedirectDevices = profile.RemoteDesktop_RedirectDevices;
+            else if (group.RemoteDesktop_OverrideRedirectDevices)
+                info.RedirectDevices = group.RemoteDesktop_RedirectDevices;
 
-            if (profileInfo.RemoteDesktop_OverrideRedirectDrives)
-                info.RedirectDrives = profileInfo.RemoteDesktop_RedirectDrives;
+            if (profile.RemoteDesktop_OverrideRedirectDrives)
+                info.RedirectDrives = profile.RemoteDesktop_RedirectDrives;
+            else if (group.RemoteDesktop_OverrideRedirectDrives)
+                info.RedirectDrives = group.RemoteDesktop_RedirectDrives;
 
-            if (profileInfo.RemoteDesktop_OverrideRedirectPorts)
-                info.RedirectPorts = profileInfo.RemoteDesktop_RedirectPorts;
+            if (profile.RemoteDesktop_OverrideRedirectPorts)
+                info.RedirectPorts = profile.RemoteDesktop_RedirectPorts;
+            else if (group.RemoteDesktop_OverrideRedirectPorts)
+                info.RedirectPorts = group.RemoteDesktop_RedirectPorts;
 
-            if (profileInfo.RemoteDesktop_OverrideRedirectSmartcards)
-                info.RedirectSmartCards = profileInfo.RemoteDesktop_RedirectSmartCards;
+            if (profile.RemoteDesktop_OverrideRedirectSmartcards)
+                info.RedirectSmartCards = profile.RemoteDesktop_RedirectSmartCards;
+            else if (group.RemoteDesktop_OverrideRedirectSmartcards)
+                info.RedirectSmartCards = group.RemoteDesktop_RedirectSmartCards;
 
-            if (profileInfo.RemoteDesktop_OverrideRedirectPrinters)
-                info.RedirectPrinters = profileInfo.RemoteDesktop_RedirectPrinters;
+            if (profile.RemoteDesktop_OverrideRedirectPrinters)
+                info.RedirectPrinters = profile.RemoteDesktop_RedirectPrinters;
+            else if (group.RemoteDesktop_OverrideRedirectPrinters)
+                info.RedirectPrinters = group.RemoteDesktop_RedirectPrinters;
 
             // Experience
-            if (profileInfo.RemoteDesktop_OverridePersistentBitmapCaching)
-                info.PersistentBitmapCaching = profileInfo.RemoteDesktop_PersistentBitmapCaching;
+            if (profile.RemoteDesktop_OverridePersistentBitmapCaching)
+                info.PersistentBitmapCaching = profile.RemoteDesktop_PersistentBitmapCaching;
+            else if (group.RemoteDesktop_OverridePersistentBitmapCaching)
+                info.PersistentBitmapCaching = group.RemoteDesktop_PersistentBitmapCaching;
 
-            if (profileInfo.RemoteDesktop_OverrideReconnectIfTheConnectionIsDropped)
-                info.ReconnectIfTheConnectionIsDropped = profileInfo.RemoteDesktop_ReconnectIfTheConnectionIsDropped;
+            if (profile.RemoteDesktop_OverrideReconnectIfTheConnectionIsDropped)
+                info.ReconnectIfTheConnectionIsDropped = profile.RemoteDesktop_ReconnectIfTheConnectionIsDropped;
+            else if (group.RemoteDesktop_OverrideReconnectIfTheConnectionIsDropped)
+                info.ReconnectIfTheConnectionIsDropped = group.RemoteDesktop_ReconnectIfTheConnectionIsDropped;
 
             // Performance
-            if (profileInfo.RemoteDesktop_OverrideNetworkConnectionType)
-                info.NetworkConnectionType = profileInfo.RemoteDesktop_NetworkConnectionType;
+            if (profile.RemoteDesktop_OverrideNetworkConnectionType)
+                info.NetworkConnectionType = profile.RemoteDesktop_NetworkConnectionType;
+            else if (group.RemoteDesktop_OverrideNetworkConnectionType)
+                info.NetworkConnectionType = group.RemoteDesktop_NetworkConnectionType;
 
-            if (profileInfo.RemoteDesktop_OverrideDesktopBackground)
-                info.DesktopBackground = profileInfo.RemoteDesktop_DesktopBackground;
+            if (profile.RemoteDesktop_OverrideDesktopBackground)
+                info.DesktopBackground = profile.RemoteDesktop_DesktopBackground;
+            else if (group.RemoteDesktop_OverrideDesktopBackground)
+                info.DesktopBackground = group.RemoteDesktop_DesktopBackground;
 
-            if (profileInfo.RemoteDesktop_OverrideFontSmoothing)
-                info.FontSmoothing = profileInfo.RemoteDesktop_FontSmoothing;
+            if (profile.RemoteDesktop_OverrideFontSmoothing)
+                info.FontSmoothing = profile.RemoteDesktop_FontSmoothing;
+            else if (group.RemoteDesktop_OverrideFontSmoothing)
+                info.FontSmoothing = group.RemoteDesktop_FontSmoothing;
 
-            if (profileInfo.RemoteDesktop_OverrideDesktopComposition)
-                info.DesktopComposition = profileInfo.RemoteDesktop_DesktopComposition;
+            if (profile.RemoteDesktop_OverrideDesktopComposition)
+                info.DesktopComposition = profile.RemoteDesktop_DesktopComposition;
+            else if (group.RemoteDesktop_OverrideDesktopComposition)
+                info.DesktopComposition = group.RemoteDesktop_DesktopComposition;
 
-            if (profileInfo.RemoteDesktop_OverrideShowWindowContentsWhileDragging)
-                info.ShowWindowContentsWhileDragging = profileInfo.RemoteDesktop_ShowWindowContentsWhileDragging;
+            if (profile.RemoteDesktop_OverrideShowWindowContentsWhileDragging)
+                info.ShowWindowContentsWhileDragging = profile.RemoteDesktop_ShowWindowContentsWhileDragging;
+            else if (group.RemoteDesktop_OverrideShowWindowContentsWhileDragging)
+                info.ShowWindowContentsWhileDragging = group.RemoteDesktop_ShowWindowContentsWhileDragging;
 
-            if (profileInfo.RemoteDesktop_OverrideMenuAndWindowAnimation)
-                info.MenuAndWindowAnimation = profileInfo.RemoteDesktop_MenuAndWindowAnimation;
+            if (profile.RemoteDesktop_OverrideMenuAndWindowAnimation)
+                info.MenuAndWindowAnimation = profile.RemoteDesktop_MenuAndWindowAnimation;
+            else if (group.RemoteDesktop_OverrideMenuAndWindowAnimation)
+                info.MenuAndWindowAnimation = group.RemoteDesktop_MenuAndWindowAnimation;
 
-            if (profileInfo.RemoteDesktop_OverrideVisualStyles)
-                info.VisualStyles = profileInfo.RemoteDesktop_VisualStyles;
-
+            if (profile.RemoteDesktop_OverrideVisualStyles)
+                info.VisualStyles = profile.RemoteDesktop_VisualStyles;
+            else if (group.RemoteDesktop_OverrideVisualStyles)
+                info.VisualStyles = group.RemoteDesktop_VisualStyles;
 
             // Set credentials
-            if (profileInfo.RemoteDesktop_UseCredentials)
+            if (profile.RemoteDesktop_UseCredentials)
             {
                 info.CustomCredentials = true;
 
-                info.Username = profileInfo.RemoteDesktop_Username;
-                info.Password = profileInfo.RemoteDesktop_Password;
+                info.Username = profile.RemoteDesktop_Username;
+                info.Password = profile.RemoteDesktop_Password;
+            }
+            else if (group.RemoteDesktop_UseCredentials)
+            {
+                info.CustomCredentials = true;
+
+                info.Username = group.RemoteDesktop_Username;
+                info.Password = group.RemoteDesktop_Password;
             }
 
             return info;
