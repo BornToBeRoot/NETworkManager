ec3f668d05aed78c30b3331096eaf54bcfc09621
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2022-09-09 01:42:15 +0200

    Feature: Add LDAP, NTP, Oracle DB

--- Source/NETworkManager.Models/Network/PortProfile.cs
+++ Source/NETworkManager.Models/Network/PortProfile.cs
@@ -7,14 +7,16 @@ namespace NETworkManager.Models.Network
         public static List<PortProfileInfo> DefaultList()
         {
             return new List<PortProfileInfo>
-            {                
+            {
                 new PortProfileInfo("Webserver", "80; 443"),
                 new PortProfileInfo("Remote access", "22; 23; 3389"),
                 new PortProfileInfo("Mailserver", "25; 110; 143; 465; 587; 993; 995"),
                 new PortProfileInfo("Filetransfer", "21; 22; 69; 989-990"),
                 new PortProfileInfo("DNS server", "53"),
-                new PortProfileInfo("Database", "1433-1434; 3306; 5432"),
+                new PortProfileInfo("Database", "1433-1434; 1521; 1830; 3306; 5432"),                
                 new PortProfileInfo("SMB", "139; 445"),
+                new PortProfileInfo("LDAP", "389; 636"),
+                new PortProfileInfo("NTP", "123"),
             };
         }
     }
