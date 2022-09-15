869f6f6cb669300db21b2e743f5802b49c5a2533
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2022-09-16 01:15:28 +0200

    Fix: Detect window resize and don't focus embedded window (#1543)
    * Fix: Detect window resize and don't focus embedded window
    
    * Fix: Don't focus embedded window on window resize

--- Source/NETworkManager/MainWindow.xaml.cs
+++ Source/NETworkManager/MainWindow.xaml.cs
@@ -428,18 +428,7 @@ namespace NETworkManager
             LoadApplicationList();
 
             // Load profiles    
-            _isProfileLoading = true;
-            ProfileFiles = new CollectionViewSource { Source = ProfileManager.ProfileFiles }.View;
-            ProfileFiles.SortDescriptions.Add(new SortDescription(nameof(ProfileFileInfo.Name), ListSortDirection.Ascending));
-            _isProfileLoading = false;
-
-            ProfileManager.OnLoadedProfileFileChangedEvent += ProfileManager_OnLoadedProfileFileChangedEvent;
-            ProfileManager.OnSwitchProfileFileViaUIEvent += ProfileManager_OnSwitchProfileFileViaUIEvent;
-
-            SelectedProfileFile = ProfileFiles.SourceCollection.Cast<ProfileFileInfo>().FirstOrDefault(x => x.Name == SettingsManager.Current.Profiles_LastSelected);
-
-            if (SelectedProfileFile == null)
-                SelectedProfileFile = ProfileFiles.SourceCollection.Cast<ProfileFileInfo>().FirstOrDefault();
+            LoadProfiles();
 
             // Hide to tray after the window shows up... not nice, but otherwise the hotkeys do not work
             if (CommandLineManager.Current.Autostart && SettingsManager.Current.Autostart_StartMinimizedInTray)
@@ -512,6 +501,20 @@ namespace NETworkManager
             // Scroll into view
             if (SelectedApplication != null)
                 ListViewApplication.ScrollIntoView(SelectedApplication);
+        }
+
+        private void LoadProfiles()
+        {
+            _isProfileLoading = true;
+            ProfileFiles = new CollectionViewSource { Source = ProfileManager.ProfileFiles }.View;
+            ProfileFiles.SortDescriptions.Add(new SortDescription(nameof(ProfileFileInfo.Name), ListSortDirection.Ascending));
+            _isProfileLoading = false;
+
+            ProfileManager.OnLoadedProfileFileChangedEvent += ProfileManager_OnLoadedProfileFileChangedEvent;
+            ProfileManager.OnSwitchProfileFileViaUIEvent += ProfileManager_OnSwitchProfileFileViaUIEvent;
+
+            SelectedProfileFile = ProfileFiles.SourceCollection.Cast<ProfileFileInfo>().FirstOrDefault(x => x.Name == SettingsManager.Current.Profiles_LastSelected);
+            SelectedProfileFile ??= ProfileFiles.SourceCollection.Cast<ProfileFileInfo>().FirstOrDefault();
         }
 
         private async void MetroWindowMain_Closing(object sender, CancelEventArgs e)
@@ -1511,13 +1514,19 @@ namespace NETworkManager
         }
         #endregion
 
-        private async void FocusEmbeddedWindow()
+        #region Focus embedded window
+        private void MetroMainWindow_Activated(object sender, EventArgs e)
         {
+            FocusEmbeddedWindow();
+        }
+
+        private async void FocusEmbeddedWindow()
+        {         
             // Delay the focus to prevent blocking the ui
             do
             {
                 await Task.Delay(250);
-            } while (Mouse.LeftButton == MouseButtonState.Pressed);
+            } while (Control.MouseButtons == MouseButtons.Left);
 
             /* Don't continue if
                - Application is not set
@@ -1525,6 +1534,7 @@ namespace NETworkManager
                - Profile file drop down is opened
                - Application search textbox is opened
                - Dialog over an embedded window is opened
+               - Window is resizing
             */
             if (SelectedApplication == null || ShowSettingsView || IsProfileFileDropDownOpened || IsTextBoxSearchFocused || ConfigurationManager.Current.FixAirspace)
                 return;
@@ -1540,10 +1550,6 @@ namespace NETworkManager
                     break;
             }
         }
-
-        private void MetroMainWindow_Activated(object sender, EventArgs e)
-        {
-            FocusEmbeddedWindow();
-        }
+        #endregion
     }
 }
