4e71bf103d85777dba0d4f1abc77b3144ea2dbbc
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2022-09-16 18:23:15 +0200

    Feature: Create IPScanner CustomCommands at first run

--- Source/NETworkManager.Models/Network/IPScanner.cs
+++ Source/NETworkManager.Models/Network/IPScanner.cs
@@ -1,6 +1,8 @@
 ﻿using DnsClient;
 using NETworkManager.Models.Lookup;
+using NETworkManager.Utilities;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
@@ -9,217 +11,231 @@ using System.Threading.Tasks;
 
 namespace NETworkManager.Models.Network
 {
-	public class IPScanner
-	{
-		#region Variables
-		private int _progressValue;
+    public class IPScanner
+    {
+        #region Variables
+        private int _progressValue;
 
-		public int Threads = 256;
-		public int ICMPTimeout = 4000;
-		public byte[] ICMPBuffer = new byte[32];
-		public int ICMPAttempts = 2;
-		public bool ResolveHostname = true;
+        public int Threads = 256;
+        public int ICMPTimeout = 4000;
+        public byte[] ICMPBuffer = new byte[32];
+        public int ICMPAttempts = 2;
+        public bool ResolveHostname = true;
 
-		public bool UseCustomDNSServer = false;
-		public IPAddress CustomDNSServer;
-		public int CustomDNSPort = 53;
-		public bool DNSUseTCPOnly = false;
-		public bool DNSUseCache = true;
-		public bool DNSRecursion = true;
-		public TimeSpan DNSTimeout = TimeSpan.FromSeconds(2);
-		public int DNSRetries = 3;
-		public bool DNSShowErrorMessage = false;
+        public bool UseCustomDNSServer = false;
+        public IPAddress CustomDNSServer;
+        public int CustomDNSPort = 53;
+        public bool DNSUseTCPOnly = false;
+        public bool DNSUseCache = true;
+        public bool DNSRecursion = true;
+        public TimeSpan DNSTimeout = TimeSpan.FromSeconds(2);
+        public int DNSRetries = 3;
+        public bool DNSShowErrorMessage = false;
 
-		public bool ResolveMACAddress = false;
-		public bool ShowScanResultForAllIPAddresses = false;
+        public bool ResolveMACAddress = false;
+        public bool ShowScanResultForAllIPAddresses = false;
 
-		private LookupClient DnsLookupClient;
-		#endregion
+        private LookupClient DnsLookupClient;
 
-		#region Events
-		public event EventHandler<HostFoundArgs> HostFound;
+        /// <summary>
+        /// Method returns a list of ip scanner custom commands (See <see cref="CustomCommandInfo"/>.
+        /// </summary>
+        /// <returns>List with <see cref="CustomCommandInfo"/>s.</returns>
+        public static List<CustomCommandInfo> GetDefaultCustomCommandList()
+        {
+            return new List<CustomCommandInfo>
+            {
+                new CustomCommandInfo(Guid.NewGuid(), "Internet Explorer", "iexplore.exe", @"http://$$ipaddress$$/"),
+                new CustomCommandInfo(Guid.NewGuid(), "Internet Explorer (https)", "iexplore.exe", @"https://$$ipaddress$$/"),
+                new CustomCommandInfo(Guid.NewGuid(), "Windows Explorer (c$)", "explorer.exe", @"\\$$ipaddress$$\c$")
+            };
+        }
+        #endregion
 
-		protected virtual void OnHostFound(HostFoundArgs e)
-		{
-			HostFound?.Invoke(this, e);
-		}
+        #region Events
+        public event EventHandler<HostFoundArgs> HostFound;
 
-		public event EventHandler ScanComplete;
+        protected virtual void OnHostFound(HostFoundArgs e)
+        {
+            HostFound?.Invoke(this, e);
+        }
 
-		protected virtual void OnScanComplete()
-		{
-			ScanComplete?.Invoke(this, EventArgs.Empty);
-		}
+        public event EventHandler ScanComplete;
 
-		public event EventHandler<ProgressChangedArgs> ProgressChanged;
+        protected virtual void OnScanComplete()
+        {
+            ScanComplete?.Invoke(this, EventArgs.Empty);
+        }
 
-		protected virtual void OnProgressChanged()
-		{
-			ProgressChanged?.Invoke(this, new ProgressChangedArgs(_progressValue));
-		}
+        public event EventHandler<ProgressChangedArgs> ProgressChanged;
 
-		public event EventHandler UserHasCanceled;
+        protected virtual void OnProgressChanged()
+        {
+            ProgressChanged?.Invoke(this, new ProgressChangedArgs(_progressValue));
+        }
 
-		protected virtual void OnUserHasCanceled()
-		{
-			UserHasCanceled?.Invoke(this, EventArgs.Empty);
-		}
-		#endregion
+        public event EventHandler UserHasCanceled;
 
-		#region Methods
-		public void ScanAsync(IPAddress[] ipAddresses, CancellationToken cancellationToken)
-		{
-			// Start the scan in a separat task
-			Task.Run(() =>
-			{
-				_progressValue = 0;
+        protected virtual void OnUserHasCanceled()
+        {
+            UserHasCanceled?.Invoke(this, EventArgs.Empty);
+        }
+        #endregion
 
-				// Create dns client and set options
+        #region Methods
+        public void ScanAsync(IPAddress[] ipAddresses, CancellationToken cancellationToken)
+        {
+            // Start the scan in a separat task
+            Task.Run(() =>
+            {
+                _progressValue = 0;
 
-				if (ResolveHostname)
-				{
-					DnsLookupClient = UseCustomDNSServer ? new LookupClient(new IPEndPoint(CustomDNSServer, CustomDNSPort)) : new LookupClient();
-					DnsLookupClient.UseCache = DNSUseCache;
-					DnsLookupClient.Recursion = DNSRecursion;
-					DnsLookupClient.Timeout = DNSTimeout;
-					DnsLookupClient.Retries = DNSRetries;
-					DnsLookupClient.UseTcpOnly = DNSUseTCPOnly;
-				}
+                // Create dns client and set options
 
-				// Modify the ThreadPool for better performance
-				ThreadPool.GetMinThreads(out var workerThreads, out var completionPortThreads);
-				ThreadPool.SetMinThreads(workerThreads + Threads, completionPortThreads + Threads);
+                if (ResolveHostname)
+                {
+                    DnsLookupClient = UseCustomDNSServer ? new LookupClient(new IPEndPoint(CustomDNSServer, CustomDNSPort)) : new LookupClient();
+                    DnsLookupClient.UseCache = DNSUseCache;
+                    DnsLookupClient.Recursion = DNSRecursion;
+                    DnsLookupClient.Timeout = DNSTimeout;
+                    DnsLookupClient.Retries = DNSRetries;
+                    DnsLookupClient.UseTcpOnly = DNSUseTCPOnly;
+                }
 
-				try
-				{
-					var parallelOptions = new ParallelOptions
-					{
-						CancellationToken = cancellationToken,
-						MaxDegreeOfParallelism = Threads
-					};
+                // Modify the ThreadPool for better performance
+                ThreadPool.GetMinThreads(out var workerThreads, out var completionPortThreads);
+                ThreadPool.SetMinThreads(workerThreads + Threads, completionPortThreads + Threads);
 
-					Parallel.ForEach(ipAddresses, parallelOptions, ipAddress =>
-					 {
-						 var pingInfo = new PingInfo();
-						 var pingable = false;
+                try
+                {
+                    var parallelOptions = new ParallelOptions
+                    {
+                        CancellationToken = cancellationToken,
+                        MaxDegreeOfParallelism = Threads
+                    };
 
-						 // PING
-						 using (var ping = new System.Net.NetworkInformation.Ping())
-						 {
-							 for (var i = 0; i < ICMPAttempts; i++)
-							 {
-								 try
-								 {
-									 var pingReply = ping.Send(ipAddress, ICMPTimeout, ICMPBuffer);
+                    Parallel.ForEach(ipAddresses, parallelOptions, ipAddress =>
+                     {
+                         var pingInfo = new PingInfo();
+                         var pingable = false;
 
-									 if (pingReply != null && IPStatus.Success == pingReply.Status)
-									 {
-										 if (ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-											 pingInfo = new PingInfo(pingReply.Address, pingReply.Buffer.Length, pingReply.RoundtripTime, pingReply.Options.Ttl, pingReply.Status);
-										 else
-											 pingInfo = new PingInfo(pingReply.Address, pingReply.Buffer.Length, pingReply.RoundtripTime, pingReply.Status);
+                         // PING
+                         using (var ping = new System.Net.NetworkInformation.Ping())
+                         {
+                             for (var i = 0; i < ICMPAttempts; i++)
+                             {
+                                 try
+                                 {
+                                     var pingReply = ping.Send(ipAddress, ICMPTimeout, ICMPBuffer);
 
-										 pingable = true;
-										 break; // Continue with the next checks...
-									 }
+                                     if (pingReply != null && IPStatus.Success == pingReply.Status)
+                                     {
+                                         if (ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                                             pingInfo = new PingInfo(pingReply.Address, pingReply.Buffer.Length, pingReply.RoundtripTime, pingReply.Options.Ttl, pingReply.Status);
+                                         else
+                                             pingInfo = new PingInfo(pingReply.Address, pingReply.Buffer.Length, pingReply.RoundtripTime, pingReply.Status);
 
-									 if (pingReply != null)
-										 pingInfo = new PingInfo(ipAddress, pingReply.Status);
-								 }
-								 catch (PingException)
-								 { }
+                                         pingable = true;
+                                         break; // Continue with the next checks...
+                                     }
 
-								 // Don't scan again, if the user has canceled (when more than 1 attempt)
-								 if (cancellationToken.IsCancellationRequested)
-									 break;
-							 }
-						 }
+                                     if (pingReply != null)
+                                         pingInfo = new PingInfo(ipAddress, pingReply.Status);
+                                 }
+                                 catch (PingException)
+                                 { }
 
-						 if (pingable || ShowScanResultForAllIPAddresses)
-						 {
-							 // DNS
-							 var hostname = string.Empty;
+                                 // Don't scan again, if the user has canceled (when more than 1 attempt)
+                                 if (cancellationToken.IsCancellationRequested)
+                                     break;
+                             }
+                         }
 
-							 if (ResolveHostname)
-							 {
-								 try
-								 {
-									 var dnsQueryResponse = DnsLookupClient.QueryReverse(ipAddress);
+                         if (pingable || ShowScanResultForAllIPAddresses)
+                         {
+                             // DNS
+                             var hostname = string.Empty;
 
-									 if (dnsQueryResponse != null && !dnsQueryResponse.HasError)
-										 hostname = dnsQueryResponse.Answers.PtrRecords().FirstOrDefault()?.PtrDomainName;
-									 else
-										 hostname = DNSShowErrorMessage ? dnsQueryResponse.ErrorMessage : "";
-								 }
-								 catch (Exception ex)
-								 {
-									 hostname = DNSShowErrorMessage ? ex.Message : "";
-								 }
-							 }
+                             if (ResolveHostname)
+                             {
+                                 try
+                                 {
+                                     var dnsQueryResponse = DnsLookupClient.QueryReverse(ipAddress);
 
-							 // ARP
-							 PhysicalAddress macAddress = null;
-							 var vendor = string.Empty;
+                                     if (dnsQueryResponse != null && !dnsQueryResponse.HasError)
+                                         hostname = dnsQueryResponse.Answers.PtrRecords().FirstOrDefault()?.PtrDomainName;
+                                     else
+                                         hostname = DNSShowErrorMessage ? dnsQueryResponse.ErrorMessage : "";
+                                 }
+                                 catch (Exception ex)
+                                 {
+                                     hostname = DNSShowErrorMessage ? ex.Message : "";
+                                 }
+                             }
 
-							 if (ResolveMACAddress)
-							 {
-								 // Get info from arp table
-								 var arpTableInfo = ARP.GetTable().FirstOrDefault(p => p.IPAddress.ToString() == ipAddress.ToString());
+                             // ARP
+                             PhysicalAddress macAddress = null;
+                             var vendor = string.Empty;
 
-								 if (arpTableInfo != null)
-									 macAddress = arpTableInfo.MACAddress;
+                             if (ResolveMACAddress)
+                             {
+                                 // Get info from arp table
+                                 var arpTableInfo = ARP.GetTable().FirstOrDefault(p => p.IPAddress.ToString() == ipAddress.ToString());
 
-								 // Check if it is the local mac
-								 if (macAddress == null)
-								 {
-									 var networkInferfaceInfo = NetworkInterface.GetNetworkInterfaces().FirstOrDefault(p => p.IPv4Address.Any(x => x.Item1.Equals(ipAddress)));
+                                 if (arpTableInfo != null)
+                                     macAddress = arpTableInfo.MACAddress;
 
-									 if (networkInferfaceInfo != null)
-										 macAddress = networkInferfaceInfo.PhysicalAddress;
-								 }
+                                 // Check if it is the local mac
+                                 if (macAddress == null)
+                                 {
+                                     var networkInferfaceInfo = NetworkInterface.GetNetworkInterfaces().FirstOrDefault(p => p.IPv4Address.Any(x => x.Item1.Equals(ipAddress)));
 
-								 // Vendor lookup
-								 if (macAddress != null)
-								 {
-									 var info = OUILookup.Lookup(macAddress.ToString()).FirstOrDefault();
+                                     if (networkInferfaceInfo != null)
+                                         macAddress = networkInferfaceInfo.PhysicalAddress;
+                                 }
 
-									 if (info != null)
-										 vendor = info.Vendor;
-								 }
-							 }
+                                 // Vendor lookup
+                                 if (macAddress != null)
+                                 {
+                                     var info = OUILookup.Lookup(macAddress.ToString()).FirstOrDefault();
 
-							 OnHostFound(new HostFoundArgs(pingInfo, hostname, macAddress, vendor));
-						 }
+                                     if (info != null)
+                                         vendor = info.Vendor;
+                                 }
+                             }
 
-						 IncreaseProcess();
-					 });
+                             OnHostFound(new HostFoundArgs(pingInfo, hostname, macAddress, vendor));
+                         }
+
+                         IncreaseProcess();
+                     });
 
 
-					OnScanComplete();
-				}
-				catch (OperationCanceledException)  // If user has canceled
-				{
-					// Check if the scan is already complete...
-					if (ipAddresses.Length == _progressValue)
-						OnScanComplete();
-					else
-						OnUserHasCanceled();
-				}
-				finally
-				{
-					// Reset the ThreadPool to default
-					ThreadPool.GetMinThreads(out workerThreads, out completionPortThreads);
-					ThreadPool.SetMinThreads(workerThreads - Threads, completionPortThreads - Threads);
-				}
-			}, cancellationToken);
-		}
+                    OnScanComplete();
+                }
+                catch (OperationCanceledException)  // If user has canceled
+                {
+                    // Check if the scan is already complete...
+                    if (ipAddresses.Length == _progressValue)
+                        OnScanComplete();
+                    else
+                        OnUserHasCanceled();
+                }
+                finally
+                {
+                    // Reset the ThreadPool to default
+                    ThreadPool.GetMinThreads(out workerThreads, out completionPortThreads);
+                    ThreadPool.SetMinThreads(workerThreads - Threads, completionPortThreads - Threads);
+                }
+            }, cancellationToken);
+        }
 
-		private void IncreaseProcess()
-		{
-			// Increase the progress                        
-			Interlocked.Increment(ref _progressValue);
-			OnProgressChanged();
-		}
-		#endregion
-	}
+        private void IncreaseProcess()
+        {
+            // Increase the progress                        
+            Interlocked.Increment(ref _progressValue);
+            OnProgressChanged();
+        }
+        #endregion
+    }
 }
