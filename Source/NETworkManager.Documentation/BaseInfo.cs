03ac51dfd8e4c3919bc8ff77a0c90d691c41c687
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2022-11-29 23:38:58 +0100

    Docs: Add documentation and remove warnings

--- Source/NETworkManager.Documentation/BaseInfo.cs
+++ Source/NETworkManager.Documentation/BaseInfo.cs
@@ -1,12 +1,31 @@
 ﻿namespace NETworkManager.Documentation
 {
-
+    /// <summary>
+    /// Base class to hold informations about a library, service or resource.
+    /// </summary>
     public abstract class BaseInfo
     {
+        /// <summary>
+        /// Name of the library, service or resource.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Url of the library, service or resource.
+        /// </summary>
         public string WebsiteUrl { get; set; }
+
+        /// <summary>
+        /// Description of the library, service or resource.
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Create an instance of <see cref="BaseInfo"/> with parameters.
+        /// </summary>
+        /// <param name="name">Name of the library, service or resource.</param>
+        /// <param name="websiteUrl">Url of the library, service or resource.</param>
+        /// <param name="description">Description of the library, service or resource.</param>
         protected BaseInfo(string name, string websiteUrl, string description)
         {
             Name = name;
--- Source/NETworkManager.Documentation/ResourceInfo.cs
+++ Source/NETworkManager.Documentation/ResourceInfo.cs
@@ -1,7 +1,16 @@
 ﻿namespace NETworkManager.Documentation
 {
+    /// <summary>
+    /// Class to hold all informations about a resource.
+    /// </summary>
     public class ResourceInfo : BaseInfo
     {
+        /// <summary>
+        /// Create an instance of <see cref="ResourceInfo"/> with parameters.
+        /// </summary>
+        /// <param name="name">Name of the resource.</param>
+        /// <param name="websiteUrl">Url of the resource.</param>
+        /// <param name="description">Description of the resource.</param>
         public ResourceInfo(string name, string websiteUrl, string description) : base(name, websiteUrl, description)
         {
             
