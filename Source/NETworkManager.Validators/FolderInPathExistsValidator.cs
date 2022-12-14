c914c0f09e7d96000a544494cbd5b96ff6ed5114
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2022-12-14 21:21:31 +0100

    Feature: Validate if folder exists for the export

---
+++ Source/NETworkManager.Validators/FolderInPathExistsValidator.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Windows.Controls;
+using NETworkManager.Localization.Resources;
+
+namespace NETworkManager.Validators
+{
+    /// <summary>
+    /// Check if the folder in the full path exists on the system (like "C:\Temp" from "C:\Temp\export.json").
+    /// </summary>
+    public class FolderInPathExistsValidator : ValidationRule
+    {
+        /// <summary>
+        /// Check if the string is a valid file path (like "C:\Temp\Test.txt"). The file path does not have to exist on the local system.
+        /// </summary>
+        /// <param name="value">File path like "C:\Temp\Test.txt"".</param>
+        /// <param name="cultureInfo">Culture to use for validation.</param>
+        /// <returns>True if the folder exists.</returns>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string path = Path.GetDirectoryName((string)value);
+
+            if (Directory.Exists(path))
+                return ValidationResult.ValidResult;
+            else
+                return new ValidationResult(false, Strings.FolderDoesNotExist);
+        }
+    }
+}
