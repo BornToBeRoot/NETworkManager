05d9eed253653a502718a1eff6a8a1bb988d589a
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2023-02-16 20:19:56 +0100

    Fix: Use UTF8 encoding for PowerShell console

--- Source/NETworkManager.Models/AWS/AWSSessionManager.cs
+++ Source/NETworkManager.Models/AWS/AWSSessionManager.cs
@@ -2,9 +2,13 @@
 {
     public static partial class AWSSessionManager
     {
+        private static string _encodingCommand = "[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding";
+        private static string _setLocationCommand = "Set-Location -Path ~";
+        private static string _clearHostCommand = "Clear-Host";
+
         public static string BuildCommandLine(AWSSessionManagerSessionInfo sessionInfo)
         {
-            var commandLine = $"-NoExit -NoLogo -NoProfile -Command \"Set-Location -Path ~; Clear-Host; aws ssm start-session --target {sessionInfo.InstanceID}";
+            var commandLine = $"-NoExit -NoLogo -NoProfile -Command \"{_encodingCommand}; {_setLocationCommand}; {_clearHostCommand}; aws ssm start-session --target {sessionInfo.InstanceID}";
 
             // Add profile
             if (!string.IsNullOrEmpty(sessionInfo.Profile))
