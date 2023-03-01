387964fb44b80727d708345b892cfcb8b8cfdeaa
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2023-03-01 22:15:20 +0100

    Chore: Refactoring

---
+++ Source/NETworkManager.Models/Network/SNMPMode.cs
@@ -0,0 +1,17 @@
+﻿namespace NETworkManager.Models.Network
+{
+    public partial class SNMP
+    {
+        #endregion
+
+        // Trap and Inform not implemented
+        public enum SNMPMode
+        {
+            Get,
+            Walk,
+            Set,
+            Trap,
+            Inform
+        }
+    }
+}
---
+++ Source/NETworkManager.Models/Network/SNMPV3AuthenticationProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace NETworkManager.Models.Network;
+
+/// <summary>
+/// Enum for the SNMP v3 authentication provider.
+/// </summary>
+public enum SNMPV3AuthenticationProvider
+{
+    MD5,
+    SHA1
+}
