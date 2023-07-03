9b29330c607015a2f8f4c240efa3f6e687368a4c
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2023-07-03 23:52:40 +0200

    Feature: Dashboard Geo & DNS Api

---
+++ Source/NETworkManager.Converters/DoubleToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NETworkManager.Converters;
+
+public sealed class DoubleToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return "-/-";
+
+        double.TryParse(value.ToString(), out var doubleValue);
+
+        return doubleValue == 0 ? "-/-" : doubleValue.ToString();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
