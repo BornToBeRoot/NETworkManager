acdc5b0191225f2c7e8ef33036c0242550cc3c91
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2023-11-15 23:52:57 +0100

    CI: Try to fix build

--- build.ps1
+++ build.ps1
@@ -67,6 +67,8 @@ if (-not(Test-Path -Path $MSBuildPath -PathType Leaf)) {
 # Build with msbuild
 Start-Process -FilePath $MSBuildPath -ArgumentList "$PSScriptRoot\Source\NETworkManager.sln /restore /t:Clean,Build /p:Configuration=Release /p:TargetFramework=$TargetFramework /p:RuntimeIdentifier=$RuntimeIdentifier /p:Platform=$TargetPlatform /p:WarningLevel=0" -Wait -NoNewWindow
 
+Start-Sleep -Seconds 10
+
 # Test if build is available
 if (-not(Test-Path -Path "$PSScriptRoot\Source\NETworkManager\bin\Release\$TargetFramework\$RuntimeIdentifier\NETworkManager.exe" -PathType Leaf)) {
     Write-Error -Message "Could not find release build. Is .NET SDK 8.0 or later installed? Check logs above for more information." -ErrorAction Stop
@@ -74,8 +76,6 @@ if (-not(Test-Path -Path "$PSScriptRoot\Source\NETworkManager\bin\Release\$Targe
 else {
     Write-Host -Object "`nBuild successful!" -ForegroundColor Green
 }
-
-Start-Sleep -Seconds 10
 
 # Create folder
 New-Item -Path "$BuildPath\NETworkManager" -ItemType Directory -Force
