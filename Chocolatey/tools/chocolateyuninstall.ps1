31956fbd2a55839ef6b954f7c5a3843eea782e6f
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2024-01-07 18:06:35 +0100

    Release: 2024.1.7.0

--- Chocolatey/tools/chocolateyuninstall.ps1
+++ Chocolatey/tools/chocolateyuninstall.ps1
@@ -7,7 +7,6 @@ $packageArgs = @{
   validExitCodes= @(0)
 }
 
-$uninstalled = $false
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
@@ -27,5 +26,5 @@ if ($key.Count -eq 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
 }
