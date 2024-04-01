6102abd89ca82327c227aec8b5809798db7312f0
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2024-04-01 22:47:57 +0200

    Chore: Code cleanup

--- Source/NETworkManager/ViewModels/IPScannerViewModel.cs
+++ Source/NETworkManager/ViewModels/IPScannerViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
