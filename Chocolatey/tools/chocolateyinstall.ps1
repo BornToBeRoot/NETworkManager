387581365a392a9ea4871e92c0cbc3479f741150
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2024-04-22 01:11:27 +0200

    Release: 2024.4.21.0

--- Chocolatey/tools/chocolateyinstall.ps1
+++ Chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/BornToBeRoot/NETworkManager/releases/download/2024.1.7.0/NETworkManager_2024.1.7.0_Setup.exe'
+$url        = 'https://github.com/BornToBeRoot/NETworkManager/releases/download/2024.4.21.0/NETworkManager_2024.4.21.0_Setup.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   url           = $url
   softwareName  = 'NETworkManager*'
 
-  checksum      = 'FE15739018AA90674FC0E0AE1BDBFB8DC015517020439C7151C3634D936FA681'
+  checksum      = 'C0A49E98C788CC0C69B0AAA87D7550B7307B797A761B8913D1CDAE305FDFF666'
   checksumType  = 'sha256'
     
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /TASKS="desktopicon" /SP-'
