ce292b6d87c38a1f8756e2d6bb55a9c27270614f
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2025-07-14 00:32:25 +0200

    Fix: 3099

---
+++ Source/NETworkManager.Converters/ChildWindowIconToRectangleStyleConverter.cs
@@ -0,0 +1,33 @@
+﻿using NETworkManager.Utilities;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NETworkManager.Converters;
+
+public sealed class ChildWindowIconToRectangleStyleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not ChildWindowIcon icon)
+            return null;
+
+        switch (icon)
+        {
+            case ChildWindowIcon.Info:
+                return Application.Current.FindResource("InfoImageRectangle");
+            case ChildWindowIcon.Warn:
+                return Application.Current.FindResource("WarnImageRectangle");
+            case ChildWindowIcon.Error:
+                return Application.Current.FindResource("ErrorImageRectangle");
+            default:
+                return null;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
