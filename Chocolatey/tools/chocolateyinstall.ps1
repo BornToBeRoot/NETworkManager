cc7598bb056fd47d86927f44fc008867acc8d62a
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2025-10-19 00:32:52 +0200

    Release: 2025.10.18.0

--- Chocolatey/tools/chocolateyinstall.ps1
+++ Chocolatey/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url            = 'https://github.com/BornToBeRoot/NETworkManager/releases/download/2025.8.10.0/NETworkManager_2025.8.10.0_Setup.msi'
+  url            = 'https://github.com/BornToBeRoot/NETworkManager/releases/download/2025.10.18.0/NETworkManager_2025.10.18.0_Setup.msi'
 
   softwareName   = 'NETworkManager*'
 
-  checksum       = '1F899FE5B3F4B162CACED4E933B3033734995D5CA6AD95F6BACE5BB5AB63A6AC'
+  checksum       = '9F139041F24829B44183A800642CC00CC615DCFC0069227696DFAA8A308C6CF7'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
