53ea91190b0ffa1a30b7deb25e6c316ea4a6fd03
Manuel Ullmann <manuel.ullmann@rediecon.com>
2026-03-02 13:55:22 +0100

    Add converter to use size factors in XAML.
    Signed-off-by: Manuel Ullmann <manuel.ullmann@rediecon.com>

---
+++ Source/NETworkManager.Converters/SizeFactorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NETworkManager.Converters;
+/// <summary>
+/// Multiplies a value by a factor given with the parameter.
+/// </summary>
+/// <remarks>
+/// Useful for setting sizes relative to window size.
+/// </remarks>
+public class SizeFactorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        try
+        {
+            double theValue = System.Convert.ToDouble(value, CultureInfo.InvariantCulture);
+            double factor = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+            return theValue * factor;
+        }
+        catch (Exception e) when (e is FormatException or InvalidCastException or OverflowException)
+        {
+            return 0.0;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
